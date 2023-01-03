684cbb4367e95b7cd2f52988325d09f32c5cc3e6
root <root@r135128.jww.swh.mhn.de>
2023-01-03 16:40:03 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,11 @@
+import torch
+
+
+def mnist():
+    # exchange with the corrupted mnist dataset
+    train = torch.randn(50000, 784)
+    test = torch.randn(10000, 784) 
+    return train, test
+
+
+change="test"
